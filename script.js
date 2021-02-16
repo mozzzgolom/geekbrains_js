a0007eb3816465e47acf0a234495786a0a0dc520
mozzzgolom <mozzzgolom@gmal.com>
2021-02-16 13:11:41 +0300

    lesson-1; add webserver

---
+++ script.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path')
+const url = require('url')
+
+const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.svg': 'image/svg+xml',
+    '.json': 'application/json',
+  }
+
+const server = http.createServer((req, res) => {    
+    // console.log(`Запрошенный адрес: ${req.url}`);
+    // получаем путь после слеша
+    const filePath = req.url.substr(1);
+    // смотрим, есть ли такой файл    
+    fs.access(filePath, fs.constants.R_OK, err => {        
+        // если произошла ошибка - отправляем статусный код 404
+        if(err){
+            res.statusCode = 404;
+            res.end("Resourse not found!");
+        } else {   
+                  
+            fs.createReadStream(filePath).pipe(res);
+            
+        }       
+      });
+});
+
+server.listen(4000)
+
+
+ // const body = req.url === '/style.css'
+    // ? fs.readFileSync('./geekbrains_js/style.css', 'utf8')
+    // : fs.readFileSync('./geekbrains_js/index.html', 'utf8')
+    // // const body = fs.readFileSync('./geekbrains_js/index.html', 'utf8');
+    // res.end(body);
+    // // console.log(req.url)
+
+  
+    
